8c7cf4128aa03245c77fbd471a65a8c1ce3c5b21
gkiplangat <gkiplangat>
2023-05-28 01:15:06 +0300

    added the getline

--- myfunctions.c
+++ myfunctions.c
@@ -13,3 +13,29 @@ void prompt(void)
 	write(STDOUT_FILENO, prompt_str, strlen(prompt_str));
 }
 
+/**
+ * readline - get user input
+ * Return: 0 on success else -1
+ */
+
+char readline(void)
+{
+	char *lineptr = NULL;
+	size_t size = 0;
+	ssize_t chars = 0;
+
+	chars = getline(&lineptr, &size, stdin);
+	if (chars == EOF)
+	{
+		free(lineptr);
+		exit(0);
+	}
+	if (lineptr == NULL)
+	{
+		write(STDOUT_FILENO, "\n", 1);
+		free(lineptr);
+		exit(-1);
+	}
+	return (lineptr);
+}
+
